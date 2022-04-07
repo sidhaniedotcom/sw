44a2d0ac08a1b347953a7c63edc66c60e5009f59
SIDHANIE <102436523+sidhaniedotcom@users.noreply.github.com>
2022-04-08 05:49:33 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -212,15 +212,9 @@ def logo():
 	auth=m+'  Created By '+k+'Sidhanie'
 	# jika ingin m3namambah kan variabel dan mengubah data variabel kitsune bisa menambahkan %s menambahkan variabel terus di ubah menjjadu string, %d = mengubah data menjadi decimal , %i = mengubah data menjadi integer
 	return '''
-%s╭━┳━╭━╭━╮%s╮╲╲╲╲╲╲%s
-%s┃┈┈┈┣▅╋▅┫┃%s╲╲╲╲╲╲%s
-%s┃┈┃┈╰━╰━━━━━━╮%s╲╲%s
-%s╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣%s╲%s
-%s╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉%s╲%s
-%s╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤%s╲%s
-%s╲┃┈┈┈┈╭━┳━━━━╯%s╲╲%s╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗
-%s╲┣━━━━━━┫%s╲╲╲╲╲╲╲%s║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝
-%s╲┃┈┈┈┈┈┈┃%s╲╲╲╲╲╲╲%s╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩  
+%s╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗
+%s║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝
+%s╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩  
 %s''' % (k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,auth)
 # -----------------------------------------------------------
 def termux():
