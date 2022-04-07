f27a25e1d8a7d312e3c2439026614388b8fc0e25
SIDHANIE <102436523+sidhaniedotcom@users.noreply.github.com>
2022-04-08 05:55:49 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -206,13 +206,7 @@ def multi():
 				print()
 		time.sleep(dly)
 	apakah()
-#-------------------------Fungsi Banner-----------------------
-def logo():
-	os.system('clear')
-	auth=m+'  Created By '+k+'Sidhanie'
-	# jika ingin m3namambah kan variabel dan mengubah data variabel kitsune bisa menambahkan %s menambahkan variabel terus di ubah menjjadu string, %d = mengubah data menjadi decimal , %i = mengubah data menjadi integer
-	
-# -----------------------------------------------------------
+
 def termux():
 	os.system('termux-contact-list > .contact')
 	po=json.loads(open('.contact','r').read())
